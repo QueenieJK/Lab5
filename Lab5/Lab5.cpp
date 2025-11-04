cfb03712d16dc233cacba335133a9b2478f25f8b
student <student@DESKTOP-5POI41D>
2025-11-04 16:25:35 +0100

    Add project files.

---
+++ Lab5/Lab5.cpp
@@ -0,0 +1,39 @@
+// Lab5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*cout << "Zadanie 1: " << endl << endl;
+    int tab[10];
+    int liczba;
+    for (int i = 0; i < 10; i += 1) {
+        cout << "Wpisz liczbe: " << endl;
+        cin >> liczba;
+        tab[i] = liczba;
+
+    }
+    for (int i = 9; i >= 0; i -= 1) {
+        cout << tab[i] << endl;
+    }*/
+    /*cout << "Zadanie 2:  " << endl << endl;
+    int a;
+    cout << "Podaj wielkoœæ tablicy" << endl;
+    cin >> a;
+    int* table = new int[a];
+    table[0] = 0;
+    table[1] = 1;
+    cout << table[0] << endl;
+    cout << table[1] << endl;
+    for (int i = 2; i < a+2; i += 1) {
+        table[i] = table[i - 1] + table[i - 2];
+        cout << table[i] << endl;
+    }*/
+    cout << "Zadanie 3: " << endl;
+    int tab[10];
+
+    
+}
+
